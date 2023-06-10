e29f43e814b5be25b5072e0f18f6184d62e10205
Farhan Mullick <38060872+Farhansadiqmullick@users.noreply.github.com>
2023-06-10 17:53:43 +0600

    script changing for email

--- assets/js/magnific-popup.js
+++ assets/js/magnific-popup.js
@@ -14,19 +14,5 @@
         duration: 300, // don't foget to change the duration also in CSS
       },
     });
-
-    function sendEmail() {
-      let name = document.getElementById("name").value;
-      let email = document.getElementById("email").value;
-      let message = document.getElementById("message").value;
-      let finalmessage = `Name : ${name} <br>  Email : ${email} <br>  Message : ${message} <br>`;
-      Email.send({
-        SecureToken: "01c2cfc6-210a-4abe-b71d-5dee3c264d55",
-        To: "farhansadiq24@gmail.com",
-        From: "info@farhanmullick.com",
-        Subject: "Email come from the website",
-        Body: finalmessage,
-      }).then((message) => alert(message));
-    }
   });
 })(jQuery);
